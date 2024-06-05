98a8b2819704b3e80b91b48c32bccc12a59877ca
Ayush Yadav <ayush421301@gmail.com>
2024-06-06 04:45:32 +0530

    cleared Component and Vite for offloading
    :wq

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,47 @@
 import React, { useState } from "react";
-import Component from "./Component"; // Import the Component component
 
 function App() {
-  const [a, fnA] = useState(0);
+  const [backgroundColor, setBackgroundColor] = useState("#2d3748");
 
-  function clickFunction() {
-    fnA(a + 1);
+  function colorChanger(color) {
+    setBackgroundColor(color);
   }
 
   return (
     <>
-      <div className="w-full h-screen bg-slate-600">
-        <Component a={a} clickFunction={clickFunction} />{" "}
-        {/* Use the Component component */}
+      <div
+        id="container"
+        className="h-screen bg-slate-800"
+        style={{ backgroundColor }}
+      >
+        <button
+          id="teal"
+          className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+          onClick={() => colorChanger("#008080")}
+        >
+          Teal
+        </button>
+        <button
+          id="magenta"
+          className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+          onClick={() => colorChanger("#ff00ff")}
+        >
+          Magenta
+        </button>
+        <button
+          id="button"
+          className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+          onClick={() => colorChanger("orangered")}
+        >
+          OrangeRed
+        </button>
+        <button
+          id="button"
+          className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+          onClick={() => colorChanger("hotpink")}
+        >
+          HotPink
+        </button>
       </div>
     </>
   );
