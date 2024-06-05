5c6db58609f20db544ea3d017aeee19a23f7cb29
Ayush Yadav <ayush421301@gmail.com>
2024-06-06 05:08:22 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,8 @@
 import React, { useState } from "react";
 
 function App() {
-  const [backgroundColor, setBackgroundColor] = useState("#2d3748");
-
-  function colorChanger(color) {
-    setBackgroundColor(color);
-  }
-
   return (
     <>
-      <div
-        id="container"
-        className="h-screen bg-slate-800"
-        style={{ backgroundColor }}
-      >
-        <button
-          id="teal"
-          className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
-          onClick={() => colorChanger("#008080")}
-        >
-          Teal
-        </button>
-        <button
-          id="magenta"
-          className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
-          onClick={() => colorChanger("#ff00ff")}
-        >
-          Magenta
-        </button>
-        <button
-          id="button"
-          className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
-          onClick={() => colorChanger("orangered")}
-        >
-          OrangeRed
-        </button>
-        <button
-          id="button"
-          className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
-          onClick={() => colorChanger("hotpink")}
-        >
-          HotPink
-        </button>
-      </div>
     </>
   );
 }
